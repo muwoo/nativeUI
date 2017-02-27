48b70b55bcebdd74a57bc98af793851772d941d1
monkeyWangs <wang.wei@loonxi.com>
2017-02-27 20:09:03 +0800

    nv-button

---
+++ packages/nativeUI.index.js
@@ -0,0 +1,4 @@
+/**
+ * Created by monkeywang on 17/2/27.
+ */
+import './style/index.less'
---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+/**
+ * @author monkeyWang
+ *
+ */
+
+/* 引入操作路径模块和webpack */
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+  /* 输入文件 */
+  entry: './packages/nativeUI.index.js',
+  output: {
+    /* 输出目录，没有则新建 */
+    path: path.resolve(__dirname, './dist'),
+    /* 静态目录，可以直接从这里取文件 */
+    publicPath: '/dist/',
+    /* 文件名 */
+    filename: 'nativeUI.js'
+  },
+  module: {
+    rules: [
+      /* 用babel来解析js文件并把es6的语法转换成浏览器认识的语法 */
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        /* 排除模块安装目录的文件 */
+        exclude: /node_modules/
+      },
+      {
+        test: /\.less$/,
+        loader: 'style-loader!css-loader!less-loader'
+      }
+    ]
+  }
+}
